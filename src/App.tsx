fcbd8c8b2426c02361dcea3e267c907d60da4708
Marcelo <marcelo.arraes@gmail.com>
2023-10-26 21:38:47 +1030

    feat: add ai package to streamline the response from openai

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,11 @@ import { Github } from "lucide-react";
 import { Separator } from "./components/ui/separator";
 import { Textarea } from "./components/ui/textarea";
 import { VideoInputForm } from "./components/video-input-form";
+import { useState } from "react";
 
 export function App() {
+  const [promptForAi, setPromptForAi] = useState("");
+
   return (
     <div className="min-h-screen flex flex-col">
       <div className="px-6 py-3 flex items-center justify-between border-b">
@@ -21,27 +24,7 @@ export function App() {
         </div>
       </div>
       <main className="flex-1 p-6 flex gap-6">
-        <div className="flex flex-col flex-1 gap-4">
-          <div className="grid grid-rows-2 gap-2 flex-1">
-            <Textarea
-              className="resize-none p-4 leading-relaxed"
-              placeholder="Prompt for IA"
-            />
-            <Textarea
-              className="resize-none p-4 leading-relaxed"
-              placeholder="Result from IA"
-            />
-          </div>
-          <p className="text-sm text-muted-foreground">
-            You can use the variable{" "}
-            <code className="text-violet-500">`{"{transcription}"}`</code>on
-            your prompt to add the content of the transcription of your selected
-            video.
-          </p>
-        </div>
-        <aside className="w-80 space-y-6">
-          <VideoInputForm />
-        </aside>
+        <VideoInputForm />
       </main>
     </div>
   );
--- src/components/video-input-form.tsx
+++ src/components/video-input-form.tsx
@@ -1,4 +1,12 @@
-import { ChangeEvent, FormEvent, useMemo, useRef, useState } from "react";
+import {
+  ChangeEvent,
+  ChangeEventHandler,
+  FormEvent,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
 import { Label } from "@/components/ui/label";
 import {
   Select,
@@ -16,6 +24,7 @@ import { loadFfmpeg } from "@/lib/ffmpeg";
 import { fetchFile } from "@ffmpeg/util";
 import { api } from "@/lib/axios";
 import { PromptSelect } from "./prompt-select";
+import { useCompletion } from "ai/react";
 
 type Status =
   | "waiting"
@@ -25,16 +34,30 @@ type Status =
   | "success"
   | "error";
 
-export const VideoInputForm = () => {
+export const VideoInputForm = ({}: {}) => {
   const [videoFile, setVideoFile] = useState<File | null>(null);
   const promptInputRef = useRef<HTMLTextAreaElement>(null);
   const [status, setStatus] = useState<Status>("waiting");
   const [temperature, setTemperature] = useState(0.5);
   const [videoId, setVideoId] = useState<string | null>(null);
 
-  const handlePromptSelected = (template: string) => {
-    console.log("handlePromptSelected ", template);
-  };
+  const {
+    input,
+    setInput,
+    handleInputChange,
+    handleSubmit,
+    completion,
+    isLoading,
+  } = useCompletion({
+    api: "http://localhost:3333/ai/complete",
+    body: {
+      videoId,
+      temperature,
+    },
+    headers: {
+      "Content-type": "application/json",
+    },
+  });
 
   const statusMessages: { [key in Status]: string } = {
     waiting: "Waiting...",
@@ -127,103 +150,126 @@ export const VideoInputForm = () => {
 
   return (
     <>
-      <form className="space-y-6" onSubmit={handleUploadVideo}>
-        <label
-          htmlFor="video"
-          className="relative border flex rounded-md aspect-video cursor-pointer border-dashed text-sm flex-col gap-2 items-center justify-center text-muted-foreground hover:bg-primary/5"
-        >
-          {previewURL ? (
-            <video
-              src={previewURL}
-              controls={false}
-              className="pointer-events-none absolute inset-0"
-            />
-          ) : (
-            <>
-              <FileVideo className="h-4 w-4" />
-              Upload Video{" "}
-            </>
-          )}
-        </label>
-        <input
-          type="file"
-          name="video"
-          onChange={handleFileSelected}
-          id="video"
-          accept="video/mp4"
-          className="sr-only"
-        />
-        <Separator />
-        <div className="space-y-4">
-          <Label htmlFor="transcription_prompt">Transcription Prompt</Label>
+      <div className="flex flex-col flex-1 gap-4">
+        <div className="grid grid-rows-2 gap-2 flex-1">
           <Textarea
-            id="transcription_prompt"
+            className="resize-none p-4 leading-relaxed"
+            placeholder="Prompt for IA"
+            value={input}
+            onChange={handleInputChange}
+          />
+          <Textarea
+            className="resize-none p-4 leading-relaxed"
+            placeholder="Result from IA"
+            value={completion}
+            readOnly
+          />
+        </div>
+        <p className="text-sm text-muted-foreground">
+          You can use the variable{" "}
+          <code className="text-violet-500">`{"{transcription}"}`</code>on your
+          prompt to add the content of the transcription of your selected video.
+        </p>
+      </div>
+      <aside className="w-80 space-y-6">
+        <form className="space-y-6" onSubmit={handleUploadVideo}>
+          <label
+            htmlFor="video"
+            className="relative border flex rounded-md aspect-video cursor-pointer border-dashed text-sm flex-col gap-2 items-center justify-center text-muted-foreground hover:bg-primary/5"
+          >
+            {previewURL ? (
+              <video
+                src={previewURL}
+                controls={false}
+                className="pointer-events-none absolute inset-0"
+              />
+            ) : (
+              <>
+                <FileVideo className="h-4 w-4" />
+                Upload Video{" "}
+              </>
+            )}
+          </label>
+          <input
+            type="file"
+            name="video"
+            onChange={handleFileSelected}
+            id="video"
+            accept="video/mp4"
+            className="sr-only"
+          />
+          <Separator />
+          <div className="space-y-4">
+            <Label htmlFor="transcription_prompt">Transcription Prompt</Label>
+            <Textarea
+              id="transcription_prompt"
+              disabled={status != "waiting"}
+              ref={promptInputRef}
+              className="h-20 leading-relaxed resize-none"
+              placeholder="Insert keywords cited on the video spaced by comma (,)"
+            />
+          </div>
+          <Button
+            data-success={status == "success"}
             disabled={status != "waiting"}
-            ref={promptInputRef}
-            className="h-20 leading-relaxed resize-none"
-            placeholder="Insert keywords cited on the video spaced by comma (,)"
-          />
-        </div>
-        <Button
-          data-success={status == "success"}
-          disabled={status != "waiting"}
-          type="submit"
-          className="w-full data-[success=true]:bg-emerald-400"
-        >
-          {status == "waiting" ? (
-            <>
-              {" "}
-              Upload Video
-              <Upload className="h-4 w-4" />
-            </>
-          ) : (
-            statusMessages[status]
-          )}
-        </Button>
-      </form>
-      <Separator />
-      <form action="" className="space-y-6">
-        <div className="space-y-2">
-          <Label>Prompt</Label>
-          <PromptSelect onPromptSelected={handlePromptSelected} />
-          <span className="block text-xs text-muted-foreground italic">
-            You can customize this option soon
-          </span>
-        </div>
-        <div className="space-y-2">
-          <Label>Model</Label>
-          <Select defaultValue="gpt3.5" disabled>
-            <SelectTrigger>
-              <SelectValue />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectItem value={"gpt3.5"}>GPT 3.5-trubo 16k</SelectItem>
-            </SelectContent>
-          </Select>
-          <span className="block text-xs text-muted-foreground italic">
-            You can customize this option soon
-          </span>
-        </div>
+            type="submit"
+            className="w-full data-[success=true]:bg-emerald-400"
+          >
+            {status == "waiting" ? (
+              <>
+                {" "}
+                Upload Video
+                <Upload className="h-4 w-4" />
+              </>
+            ) : (
+              statusMessages[status]
+            )}
+          </Button>
+        </form>
         <Separator />
-        <div className="space-y-4">
-          <Label>Temperature</Label>
-          <Slider
-            min={0}
-            max={1}
-            step={0.1}
-            value={[temperature]}
-            onValueChange={(value) => setTemperature(value[0])}
-          />
-          <span className="block text-xs text-muted-foreground italic leading-relaxed">
-            You can customize this option soon
-          </span>
-        </div>
-        <Separator />
-        <Button type="submit">
-          Play
-          <Wand2 className="h-4 w-4 " />
-        </Button>
-      </form>
+        <form onSubmit={handleSubmit} action="" className="space-y-6">
+          <div className="space-y-2">
+            <Label>Prompt</Label>
+            <PromptSelect onPromptSelected={setInput} />
+            <span className="block text-xs text-muted-foreground italic">
+              You can customize this option soon
+            </span>
+          </div>
+          <div className="space-y-2">
+            <Label>Model</Label>
+            <Select defaultValue="gpt3.5" disabled>
+              <SelectTrigger>
+                <SelectValue />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value={"gpt3.5"}>GPT 3.5-trubo 16k</SelectItem>
+              </SelectContent>
+            </Select>
+            <span className="block text-xs text-muted-foreground italic">
+              You can customize this option soon
+            </span>
+          </div>
+          <Separator />
+          <div className="space-y-4">
+            <Label>Temperature</Label>
+            <Slider
+              min={0}
+              max={1}
+              step={0.1}
+              value={[temperature]}
+              onValueChange={(value) => setTemperature(value[0])}
+            />
+            <span className="block text-xs text-muted-foreground italic leading-relaxed">
+              You can customize this option soon
+            </span>
+          </div>
+          <Separator />
+          <Button type="submit" disabled={isLoading}>
+            Play
+            <Wand2 className="h-4 w-4 " />
+          </Button>
+        </form>
+      </aside>
     </>
   );
 };
